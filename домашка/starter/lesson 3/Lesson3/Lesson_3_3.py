bc96302b5116eb20ecc74ef51c24b63431d01866
denik-mv <56339338+denik-mv@users.noreply.github.com>
2019-11-16 21:47:59 +0200

    Delete Lesson_3_3.py

--- домашка/starter/lesson 3/Lesson3/Lesson_3_3.py
+++
@@ -1,9 +0,0 @@
-from math import pi, cos
-
-x = int(input('Enter x for system of equations: '))
-if -pi <= x <= pi:
-    y = cos(3*x)
-    print ('Solution of y = cos({}) is {:4.2f}'.format(3*x, y))
-elif (x < -pi) or (x > pi):
-    y = x
-    print(f'Solution of y = x is {y}')
