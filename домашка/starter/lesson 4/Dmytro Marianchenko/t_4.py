42f9e094704e5f953552efb260eb486aee8b62e2
dimamarjan <dima.marjan@gmail.com>
2019-11-20 23:51:10 +0200

    create

---
+++ домашка/starter/lesson 4/Dmytro Marianchenko/t_4.py
@@ -0,0 +1,10 @@
+x = int(input("Введите значение ШИРИНУ: "))
+y = int(input("Введите значение ВЫСОТУ: "))
+print()
+print()
+while y != 0:
+    print("* "*x)
+    y -= 1
+print()
+print()
+print("Наш прямоугольник!")
