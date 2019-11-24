510b2003f0ea70f62c7c401957fd94f18843dee9
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2019-11-24 21:41:39 +0200

    Add files via upload

---
+++ домашка/starter/lesson 6/Pavel K/hw_6_4.py
@@ -0,0 +1,10 @@
+def sum(a):
+    if a == b:
+        return (a)
+    return sum(a+1)+a
+
+
+a = int(input('Введите 1-е число'))
+b = int(input('Введите 2-е число'))
+t = sum(a)
+print(f'сумма ряда натуральных чисел между ними {t}')
