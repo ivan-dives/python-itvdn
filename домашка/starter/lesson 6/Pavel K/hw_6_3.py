1e579dd41ce64e1ec108fa3b73bc8041f42d5f01
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2019-11-24 16:21:50 +0200

    Add files via upload

---
+++ домашка/starter/lesson 6/Pavel K/hw_6_3.py
@@ -0,0 +1,12 @@
+import functools
+@functools.lru_cache(maxsize=None)
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+print(fib(input('put number Fibonacci')))
