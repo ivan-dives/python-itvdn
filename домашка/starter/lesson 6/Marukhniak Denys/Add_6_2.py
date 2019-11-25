3e3b3bb77a3981469eb5430dbe2d54f78d63d62f
Ivan Diorditsa <ivan.diorditsa@gmail.com>
2019-11-25 21:36:45 +0200

    update

--- домашка/starter/lesson 6/Marukhniak Denys/Add_6_2.py
+++ домашка/starter/lesson 6/Marukhniak Denys/Add_6_2.py
@@ -1,18 +1,32 @@
 import os
+import pathlib
 import stat
 
+#PATH = "D:\Python\Starter"
+PATH = pathlib.Path(os.getcwd()) / '..'
+os.chdir(PATH)
 
-def path_tree(path):
-    types = os.lstat(path)
+
+def path_tree(p):
+    for f in os.listdir(p):
+        f = p / f
+        s = os.lstat(f)
+        if stat.S_ISDIR(s.st_mode):
+            path_tree(f)
+        else:
+            print(f)
+
+    return
+
+    types = os.lstat(p)
     folder = stat.S_ISDIR(types.st_mode)  # вернёт True если папка
     if folder:
-        path = path + f'\\{os.listdir(path=path)[0]}'
+
+        #path = path + f'\\{os.listdir(path=path)[0]}'
         path_tree(path)
         return path
     else:
         print(path)
         return
 
-
-PATH = "D:\Python\Starter"
 path_tree(PATH)
