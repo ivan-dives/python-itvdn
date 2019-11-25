1d5fc50b4d755077160f4fa024e0db9a82659843
Ivan Diorditsa <ivan.diorditsa@gmail.com>
2019-11-25 21:23:24 +0200

    update

--- домашка/starter/lesson 6/Pavel K/hw_6_6.py
+++ домашка/starter/lesson 6/Pavel K/hw_6_6.py
@@ -1,16 +1,25 @@
+import pathlib
 import os
-path = 'D:\\dirforrange' #В виде строки храниться путь к нашей корневой папке
+
+#PATH = pathlib.Path('D:\\dirforrange')
+PATH = pathlib.Path('/home/ivan/PycharmProjects/python-itvdn/домашка/starter/lesson 6')
+#path = 'D:\\dirforrange' #В виде строки храниться путь к нашей корневой папке
+
 
 def obxoddir(path):
     for i in os.listdir(path): # обходим все элементы папки
-        if os.path.isdir(path +'\\' + i):
-            createtxt(path + '\\' + i)
+        p = path / i
+        if os.path.isdir(p):
+            createtxt(p)
+
 
 def createtxt(way):
-    s1 = open(str(way) +'\\'+ '1.txt', 'w')
-    print(way + '.txt')
-    k = 'Все права принадлежат ___ номер для связи тел...!'
-    s1.write(k)
-    s1.close()
+    f = way / '1.txt'
+    with open(f, 'w') as s1:
+        print(f)
+        k = 'Все права принадлежат ___ номер для связи тел...!'
+        s1.write(k)
     obxoddir(way)
-obxoddir(path)
+
+
+obxoddir(PATH)
