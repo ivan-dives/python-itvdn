f1362cd30777902d741714e0c5e773e42edd4511
denik-mv <levyiakk123@gmail.com>
2019-11-26 15:53:25 +0200

    doctest works

--- домашка/starter/lesson 6/Marukhniak Denys/Lesson_6_3.py
+++ домашка/starter/lesson 6/Marukhniak Denys/Lesson_6_3.py
@@ -18,12 +18,18 @@ def stair(stair_n):
     >>> stair(4)
     5
     """
-    if stair_n in range(-1, 2):
-        return stair_n + 1
+    def stair_s(stair_num):
+        if stair_num in range(0, 2):
+            return stair_num
+        else:
+            return stair_s(stair_num-1) + stair_s(stair_num-2)
+
+    if stair_n == 0:
+        return 0
     else:
-        return stair(stair_n-1) + stair(stair_n-2)
+        return stair_s(stair_n+1)
 
 
 doctest.run_docstring_examples(stair, None, verbose=True)
 n = int(input('Enter the number of stair you want to climb: '))
-print('Result: ', stair(n-1))
+print('Result: ', stair(n))
