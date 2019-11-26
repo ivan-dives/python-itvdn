1039ad879bdddb068a9ac38776e4b97c66cd6178
Auspex <vio.auspex@gmail.com>
2019-11-26 16:45:35 +0200

    hanoi_v2(moves counter)

--- домашка/starter/lesson 6/Vitalii_K/hanoi.py
+++ домашка/starter/lesson 6/Vitalii_K/hanoi.py
@@ -1,10 +1,17 @@
-def hanoi(height = 1, rod1 = 1, rod3 = 3):
-    if height == 1:
-        print(f'Move disk from <<rod {rod1}>> to <<rod {rod3}>>')
-    else:
-        rod2 = 6 - rod1 - rod3
-        hanoi(height - 1, rod1, rod2)
-        print(f'Move disk from <<rod {rod1}>> to <<rod {rod3}>>')
-        hanoi(height - 1, rod2, rod3)
-h = int(input('Tower height: '))
-hanoi(h)
+def hanoi(h = 1, r_def = 1, r_tar = 3, r_tmp = 2):
+    n = 0
+    def hanoi_inner(h, r_def, r_tar, r_tmp):
+        nonlocal n
+        if h > 0:
+            hanoi_inner(h - 1, r_def, r_tmp, r_tar)
+            print(f'Move disk from <<{r_def}>> \tto\t <<{r_tar}>>')
+            hanoi_inner(h - 1, r_tmp, r_tar, r_def)
+            n +=1
+    hanoi_inner(h, r_def, r_tar, r_tmp)
+    return n
+height = int(input('Tower height:'))
+rdef = '1st Rod'
+rtar = '3rd Rod'
+rtmp = '2nd Rod'
+moves = hanoi(height, rdef, rtar, rtmp)
+print(f'Optimal moves count is: {moves}')
