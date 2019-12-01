08c361442ef0d4c8ad9a94c9b867e806e00e674f
denik-mv <levyiakk123@gmail.com>
2019-12-01 22:31:46 +0200

    Editor

---
+++ домашка/essential/lesson 2/Marukhniak Denys/Lesson_e_2_1.py
@@ -0,0 +1,47 @@
+# Задание 1
+# Создайте класс Editor, который содержит методы view_document и edit_document. Пусть метод
+# edit_document выводит на экран информацию о том, что редактирование документов недоступно для
+# бесплатной версии. Создайте подкласс ProEditor, в котором данный метод будет переопределён.
+# Введите с клавиатуры лицензионный ключ и, если он корректный, создайте экземпляр класса ProEditor,
+# иначе Editor. Вызовите методы просмотра и редактирования документов.
+
+
+class Editor:
+    document = "Текущий текст"
+
+    def view_document(self):
+        print(f'Текущий текст:\n'
+              f'{self.document}')
+
+    def edit_document(self):
+        print('Редактирование документов недоступно для бесплатной версии')
+
+
+class ProEditor(Editor):
+
+    def edit_document(self):
+        print('Редактирование документов разрешено')
+        do = input('Хотите ввести другой текст (Yes/No)?')
+        if do.lower() == 'yes':
+            new_doc = input('Введите новый текст:')
+            self.document = new_doc
+        else:
+            do = input('Хотите дописать текст (Yes/No)?')
+            if do.lower() == 'yes':
+                new_doc = input('Введите текст:')
+                self.document += new_doc
+        print(f'Новый текст:\n'
+              f'{self.document}')
+
+
+license_pass = input('Введите лицензионный ключ: ')
+if license_pass == 'password':
+    print('Введен верный лицензионный ключ')
+    doc = ProEditor()
+    doc.view_document()
+    doc.edit_document()
+else:
+    print('Введен неверный лицензионный ключ')
+    doc = Editor()
+    doc.view_document()
+    doc.edit_document()
