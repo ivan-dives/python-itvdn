b6f2912377afe882188210e2e58eef9f1e7a01f1
Александр <apamic1995@gmail.com>
2019-12-04 20:03:16 +0200

    Vehicle

--- домашка/essential/lesson 2/Alex Shvab/HW4.py
+++ домашка/essential/lesson 2/Alex Shvab/HW4.py
@@ -1,26 +1,60 @@
 class Vehicle:
-    def __init__(self, wheels=4, carcase="Доска"):
+    def __init__(self, wheels=4, transport="Средство передвижения"):
         self.wheels = wheels
-        self.carcase = carcase
+        self.transport = transport
 
     def go(self):
-        print(f"{self.carcase} на {self.wheels} колёсах")
+        print(f"{self.transport} на {self.wheels} колёсах")
 
 class Car(Vehicle):
-    def __init__(self, wheels=4, engine=1, carcase="Кузов"):
+    def __init__(self, wheels=4, engine=1, transport="Машина"):
         self.wheels = wheels
-        self.carcase = carcase
+        self.transport = transport
         self.engine = engine
 
     def go(self):
-        print(f"{self.carcase} на {self.wheels} колесах и {self.engine} двигателем")
+        print("Поехали")
+        print(f"{self.transport} на {self.wheels} колесах и {self.engine} двигателем")
 
 class Bus(Car):
-    def __init__(self, wheels=4, passengers=9, engine=1, carcase="Кузов"):
+    def __init__(self, wheels=4, passengers=9, engine=1, transport="Автобус"):
         self.wheels = wheels
-        self.carcase = carcase
+        self.transport = transport
         self.engine = engine
         self.passengers = passengers
 
     def go(self):
-        print(f"{self.carcase} на {self.wheels} и {self.engine} перевозящий {self.passengers} пасажиров")
+        print("Поехали")
+        print(f"{self.transport} на {self.wheels} кольосах и {self.engine} \
+        перевозящий {self.passengers} пасажиров")
+
+class Boat(Vehicle):
+    def __init__(self, wheels=0, passengers=2, transport="Лодка"):
+        self.wheels = wheels
+        self.transport = transport
+        self.passengers = passengers
+
+
+    def go(self):
+        print("Кузмич бери весла и греби")
+        print(f"{self.transport} на {self.wheels} кольосами и перевозящий {self.passengers} пасажиров")
+
+
+class Plane(Vehicle):
+    def __init__(self, wings=2, passengers=50, engine=2, transport="Самолет"):
+        self.wings = wings
+        self.transport = transport
+        self.passengers = passengers
+        self.engine = engine
+
+    def go(self):
+        print("Полетели")
+        print(f"{self.transport} с {self.wings} крыльями, {self.engine} двигателей, и перевозящий {self.passengers} пасажиров")
+
+
+reno = Car()
+reno.go()
+boing = Plane()
+boing.go()
+rubber_boat = Boat()
+rubber_boat.go()
