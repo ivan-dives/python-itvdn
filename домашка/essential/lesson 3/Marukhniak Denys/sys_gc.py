71cbca88c54efe96cbd1691576960dab50568022
denik-mv <levyiakk123@gmail.com>
2019-12-08 23:19:57 +0200

    practice from skype 08.12.19

--- домашка/essential/lesson 3/Marukhniak Denys/sys_gc.py
+++ домашка/essential/lesson 3/Marukhniak Denys/sys_gc.py
@@ -7,5 +7,7 @@ import gc
 i = 'abcde'
 q1 = sys.getrefcount(i)  # return the reference count for an object (plus one :-)
 print(q1)
+print(type(q1))
 q2 = gc.get_referrers(i)  # Return the list of objects that refer to an object.
 print(q2)
+print(type(q2))
