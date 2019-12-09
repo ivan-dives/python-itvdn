ce40fbe1fe8e4b327803baaa504c202afc50af62
denik-mv <levyiakk123@gmail.com>
2019-12-09 13:25:39 +0200

    calculator with exceptions

---
+++ домашка/essential/lesson 3/Marukhniak Denys/L_3_2.py
@@ -0,0 +1,32 @@
+# Задание 2
+# Напишите программу-калькулятор, которая поддерживает следующие операции: сложение, вычитание,
+# умножение, деление и возведение в степень. Программа должна выдавать сообщения об ошибке и
+# продолжать работу при вводе некорректных данных, делении на ноль и возведении нуля в
+# отрицательную степень.
+
+
+while True:
+    op = input('Enter operation (+, -, *, /, ^) or write (end) to interrupt calculation: ')
+    if op in ['+', '-', '*', '/']:
+        a = int(input('First: '))
+        b = int(input('Second: '))
+        if op == '+':
+            print(f'Result {a} + {b} = {a+b}')
+        if op == '-':
+            print(f'Result {a} - {b} = {a-b}')
+        if op == '*':
+            print(f'Result {a} * {b} = {a*b}')
+        if op == '/':
+            try:
+                print(f'Result {a} / {b} = {a/b}')
+            except ZeroDivisionError as zer0_div:
+                print('Division by zero')
+    elif op == '^':
+        c = int(input('Enter a number: '))
+        d = int(input('Enter a power: '))
+        try:
+            print(f'Result of {c}^{d} = {c ** d}')
+        except ZeroDivisionError as zer0_pow:
+            print('Power of zero cannot be a negative number')
+    elif op == 'end':
+        break
