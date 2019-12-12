fc939a0046fc01cad63bb419904e12b7e5eeadcb
Aleksandr Osmołowski <osm@ukr.net>
2019-12-12 15:05:02 +0200

    второе задание

--- домашка/essential/lesson 4/Alex Osmolowski/hw_04_02.py
+++ домашка/essential/lesson 4/Alex Osmolowski/hw_04_02.py
@@ -1,37 +1,30 @@
 # Задание 1
 # Напишите итератор, который возвращает элементы заданного списка в обратном порядке (аналог
 # reversed).
+# Задание 2
+# Перепишите решение первого задания с помощью генератора.
 
-class MyRevers(object):
-    """Итератор, возвращающий последовательность в обратном порядке"""
 
-    def __init__(self, seq):
-        self.seq = seq
-        self.ind = len(seq) - 1
+def my_revers(seq):
+    """Генератор, возвращающий последовательность в обратном порядке"""
 
-    def __iter__(self):
-        return self
+    idx = len(seq) - 1
 
-    def __next__(self):
-        if self.ind < 0:
-            raise StopIteration
-
-        value = self.seq[self.ind]
-        self.ind -= 1
-
-        return value
+    while idx >= 0:
+        yield seq[idx]
+        idx -= 1
 
 
 def main():
     # Перевернём строку
     my_str = "УЛЫБОК ТЕБЕ ДЕД МАКАР"
-    for i in MyRevers(my_str):
+    for i in my_revers(my_str):
         print(i, end="")
     print()
 
     # Перевернём последовательность степеней двойки
     my_seq = [1, 2, 4, 8, 16, 35, 64, 128, 256, 512, 1024]
-    for i in MyRevers(my_seq):
+    for i in my_revers(my_seq):
         print(i, end='  ')
 
 
