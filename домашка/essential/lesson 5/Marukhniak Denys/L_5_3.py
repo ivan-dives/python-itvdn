0a3a3fa2a26ccebca76e0387946453b53ec3fba4
denik-mv <levyiakk123@gmail.com>
2019-12-13 20:47:39 +0200

    sort text

---
+++ домашка/essential/lesson 5/Marukhniak Denys/L_5_3.py
@@ -0,0 +1,31 @@
+# Задание 3
+# Напишите программу, которая вводит с клавиатуры текст и выводит отсортированные по алфавиту слова
+# данного текста.
+
+
+text = input('Enter a text: ')
+
+tmp_str = [['']]
+tmp = []
+i = 0
+still_is_space = False
+new_str = ''
+
+for char in text:
+    if not char.isspace():
+        still_is_space = False
+        tmp_str[i][0] += char
+    elif still_is_space is True:
+        pass
+    else:
+        tmp_str.append([''])
+        i += 1
+        still_is_space = True
+
+for x in range(len(tmp_str)):
+    tmp.append(sorted(tmp_str[x][0]))
+    for y in tmp[x]:
+        new_str += y
+    new_str += ' '
+
+print(f'Sorted text: {new_str}')
