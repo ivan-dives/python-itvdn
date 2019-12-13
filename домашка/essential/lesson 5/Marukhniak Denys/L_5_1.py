183497d26c98943cc87a1c7a93f84c3e6033c429
denik-mv <levyiakk123@gmail.com>
2019-12-13 15:57:00 +0200

    average

---
+++ домашка/essential/lesson 5/Marukhniak Denys/L_5_1.py
@@ -0,0 +1,15 @@
+# Задание 1
+# Создайте функцию от произвольного количества аргументов, которая вычисляет среднее
+# арифметическое данных чисел. Вычислите при помощи неё среднее арифметическое двух заданных
+# чисел и среднее арифметическое чисел из заданного диапазона.
+
+
+def avg(*args):
+    return sum(args)/len(args)
+
+
+numbs = input('Enter numbers separated by one space: ')
+numbs = numbs.split(sep=' ')
+numbs = [int(i) for i in numbs]
+print('Average of', numbs, '= ', end='')
+print(avg(*numbs))
