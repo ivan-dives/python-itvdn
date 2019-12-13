bba668aaf228a05493d07421968505ad8c0a9e74
denik-mv <levyiakk123@gmail.com>
2019-12-13 22:41:19 +0200

    sort text update

--- домашка/essential/lesson 5/Marukhniak Denys/L_5_3.py
+++ домашка/essential/lesson 5/Marukhniak Denys/L_5_3.py
@@ -5,7 +5,7 @@
 
 text = input('Enter a text: ')
 
-tmp_str = [['']]
+tmp_str = ['']
 tmp = []
 i = 0
 still_is_space = False
@@ -14,16 +14,16 @@ new_str = ''
 for char in text:
     if not char.isspace():
         still_is_space = False
-        tmp_str[i][0] += char
+        tmp_str[i] += char
     elif still_is_space is True:
         pass
     else:
-        tmp_str.append([''])
+        tmp_str.append('')
         i += 1
         still_is_space = True
 
 for x in range(len(tmp_str)):
-    tmp.append(sorted(tmp_str[x][0]))
+    tmp.append(sorted(tmp_str[x]))
     for y in tmp[x]:
         new_str += y
     new_str += ' '
