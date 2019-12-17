da978f02e0bde529f924384801f50f9e2a1f8c38
Aleksandr Osmołowski <osm@ukr.net>
2019-12-17 16:10:17 +0200

    создаю папку для ДЗ

---
+++ домашка/essential/lesson 5/Alex Osmolowski/hw_05_02.py
@@ -0,0 +1,15 @@
+# Используя документацию, ознакомьтесь с методами класса str.
+
+
+def main():
+    print(dir(str))
+    print(help("".casefold))
+    print(help("".replace))
+    print(help("".upper))
+    print(help("".lower))
+    print(help("".split))
+
+
+
+if __name__ == '__main__':
+    main()
