0da485cfff25b4f2fa28da1fd8ba0bad21ec0d79
dimamarjan <dima.marjan@gmail.com>
2019-12-19 11:21:58 +0200

    update

--- домашка/essential/lesson 6/Dmytro Marianchenko/t_4.py
+++ домашка/essential/lesson 6/Dmytro Marianchenko/t_4.py
@@ -5,5 +5,14 @@ def base(**kwargs):
 
 param = {"Toyota": 25, "Mazda": 30, "Mitsubishi": 15}
 base_res = base(**param)
-print(base_res)
+print(*base_res)
+print()
+for key, values in base_res.items():
+    print(key)
+print()
+for key, values in base_res.items():
+    print(values)
+print()
+for key, values in base_res.items():
+    print(f"{key} in stock {values} cars")
 
