5b5a39ff24b7f71041d53c84d82c37f428b2f39d
OxanaKuvyrkova <57861693+OxanaKuvyrkova@users.noreply.github.com>
2019-12-20 00:40:51 +0200

    Add files via upload

---
+++ домашка/essential/lesson 6/Кувыркова Оксана/same_is_str.py
@@ -0,0 +1,14 @@
+
+
+
+#
+# garden_1 = {'apple', "pear", 'appricot', 'peach'}
+# garden_2 = {'kiwi', 'apple', 'banana', 'mango'}
+#
+# print(garden_1 & garden_2)
+
+garden_1 = set(input('Enter any text here\n'))
+
+garden_2 = set(input('Enter any other text here\n'))
+
+print(garden_1 & garden_2)
