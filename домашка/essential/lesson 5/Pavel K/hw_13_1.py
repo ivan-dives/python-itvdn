4b65b2d97b39b87c6fa8b54358a81d045b323575
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2019-12-21 14:50:26 +0200

    Update hw_13_1.py

--- домашка/essential/lesson 5/Pavel K/hw_13_1.py
+++ домашка/essential/lesson 5/Pavel K/hw_13_1.py
@@ -3,3 +3,14 @@ def foo (z):
 
 z = tuple(map(int,input().split()))
 print(foo(z))
+
+______________________________
+
+def foo(*args):
+    return sum(args)/len(args)
+
+y_nam = input('Enter your numbers: ')
+y_nam = y_nam.split()
+y_nam = [int(i) for i in y_nam]
+
+print(foo(*y_nam))
