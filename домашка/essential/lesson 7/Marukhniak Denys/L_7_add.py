250201b05aeb28fae9920ad66ed986e3657b7924
denik-mv <levyiakk123@gmail.com>
2019-12-22 18:30:53 +0200

    импорт с другой папки

--- домашка/essential/lesson 7/Marukhniak Denys/L_7_add.py
+++ домашка/essential/lesson 7/Marukhniak Denys/L_7_add.py
@@ -4,17 +4,14 @@
 import os
 import pprint
 import sys
-# import L_4_add
 
 
 def main():
     lib_path = os.path.abspath(os.path.join(__file__, '..', '..', '..', 'lesson 4', 'Marukniak Denys'))
     sys.path.insert(0, lib_path)
-
-    # for x in L_4_add.prime_number(11):
-    #     print(x, end=' ')
-    pprint.pprint(sys.path)
-    print(os.path.join(__file__))
+    import L_4_add
+    # pprint.pprint(sys.path)
+    # print(os.path.join(__file__))
 
 
 if __name__ == '__main__':
