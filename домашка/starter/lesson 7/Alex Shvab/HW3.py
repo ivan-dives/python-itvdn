ee640fc2d667757a7bb155e21060c585d578b2e4
Александр <apamic1995@gmail.com>
2019-12-23 12:25:55 +0200

    Simple numb

--- домашка/starter/lesson 7/Alex Shvab/HW3.py
+++ домашка/starter/lesson 7/Alex Shvab/HW3.py
@@ -1,5 +1,5 @@
-__all__ = ["simpl_numb", "list"]
 list = []
+
 
 def simpl_numb(n):
     for i in range(2, n):
@@ -15,24 +15,25 @@ simpl_numb(x+1)
 print(f"Simple numbers: {list}")
 
 
-while True:
-    print("""Enter operation:
-    1. Sum
-    2. Mult
-    3. Exit""")
-    op = int(input("> "))
-    if op == 1:
-        sum = 0
-        for i in range( len(list)):
-            sum = sum + list[i]
-        print(sum)
-    elif op == 2:
-        sum = 1
-        for i in range( len(list)):
-            sum = sum * list[i]
-        print(sum)
-    elif op == 3:
-        break
+def main():
+    while True:
+        print("""Enter operation:
+        1. Sum
+        2. Mult
+        3. Exit""")
+        op = int(input("> "))
+        if op == 1:
+            sum = 0
+            for i in range( len(list)):
+                sum = sum + list[i]
+            print(sum)
+        elif op == 2:
+            sum = 1
+            for i in range( len(list)):
+                sum = sum * list[i]
+            print(sum)
+        elif op == 3:
+            break
 
 if __name__ == "__main__":
     main()
