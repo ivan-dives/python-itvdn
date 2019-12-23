0b82fbbc6a0e0ce2c4b4bc118f027c270f47806b
Александр <apamic1995@gmail.com>
2019-12-23 12:35:34 +0200

    Simpl numb with module

--- домашка/essential/lesson 7/Alex Shvab/HW3.py
+++ домашка/essential/lesson 7/Alex Shvab/HW3.py
@@ -1,8 +1,15 @@
-from HW3 import *
 import sys
+import os
+import pathlib
+p = pathlib.Path(os.getcwd())
+p /= "../../../starter/lesson 7/Alex Shvab"
+p = p.resolve()
 
-sys.path.insert(0, '/starter/lesson7/Alex Shvab/')
+sys.path.insert(0, str(p))
 
-x = int(input("Enter count of simple number: "))
+
+import HW3
+
+x = int(input("Enter range of simple number: "))
 HW3.simpl_numb(x+1)
 print((HW3.list))
