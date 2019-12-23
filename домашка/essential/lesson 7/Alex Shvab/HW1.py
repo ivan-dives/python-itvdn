97256e09e200930a076319ae6399f59a495ce916
Александр <apamic1995@gmail.com>
2019-12-23 12:11:25 +0200

    Short link

--- домашка/essential/lesson 7/Alex Shvab/HW1.py
+++ домашка/essential/lesson 7/Alex Shvab/HW1.py
@@ -1,21 +1,21 @@
 import url_work
 
-
-while True:
-    print("""Select operation:
-    1. Take short link
-    2. Take original link
-    3. Exit""")
-    choose = int(input("> "))
-    if choose == 1:
-        enter_full_url = input("Enter your url: ")
-        print("Short link: ", url_work.cut_link(enter_full_url))
-    elif choose == 2:
-        short_link = input("Enter your short link: ")
-        print("Full link: ", url_work.full_link(short_link))
-    else:
-        print("Exit")
-        break
+def main():
+    while True:
+        print("Choose operation:")
+        print("1. Take short link")
+        print("2. Take original link")
+        print("3. Exit")
+        choose = int(input("> "))
+        if choose == 1:
+            enter_full_url = input("Enter your url: ")
+            print("Short link: ", url_work.cut_link(enter_full_url))
+        elif choose == 2:
+            short_link = input("Enter your short link: ")
+            print("Full link: ", url_work.full_link(short_link))
+        else:
+            print("Exit")
+            break
 
 if __name__ == "__main__":
     main()
