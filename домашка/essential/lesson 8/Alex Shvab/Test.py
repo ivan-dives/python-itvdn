5db14b0221793e8c69ad99e027e6cb21405e204d
Александр <apamic1995@gmail.com>
2019-12-25 12:49:14 +0200

    filtr

--- домашка/essential/lesson 8/Alex Shvab/Test.py
+++ домашка/essential/lesson 8/Alex Shvab/Test.py
@@ -1,6 +1,5 @@
-x = y = [1, 2, 3]
-x = x + [4]
-print(x == y)
-a = b = [1, 2, 3]
-a += [4]
-print(a == b)
+lst = [-2, 0, 1, 4, 17, 5, -8, 64, 9, 72, -3]
+print(lst)
+odd = filter(lambda x: x % 2 == 1, lst)
+res = map(lambda x: x ** 2, odd)
+print(list(res))
