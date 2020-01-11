9ffba7086432843e8b0e945dfa49278b2e93e826
Aleksandr Osmołowski <osm@ukr.net>
2020-01-11 20:11:24 +0200

    создаю папку для ДЗ

--- домашка/essential/lesson 9/Alex Osmolowski/hw_09_03.py
+++ домашка/essential/lesson 9/Alex Osmolowski/hw_09_03.py
@@ -3,8 +3,22 @@
 # последовательности только чётные числа.
 
 
+def even_numbers(generator):
+    def decorated_sequence(*args):
+        seq = generator(*args)
+        return filter(lambda x: x % 2 == 0, seq)
+    return decorated_sequence
+
+
+@even_numbers
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+        yield b
+
 def main():
-    pass
+    print(list(fibonacci(20)))
 
 
 if __name__ == '__main__':
