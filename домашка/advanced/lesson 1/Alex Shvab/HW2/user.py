317b813ee3cb662e2dd33f1d07ff703534dbf040
Александр <apamic1995@gmail.com>
2020-01-15 15:32:15 +0200

    User

---
+++ домашка/advanced/lesson 1/Alex Shvab/HW2/user.py
@@ -0,0 +1,8 @@
+import socket
+
+
+user = user = socket.gethostname()
+user = user.encode("utf-8")
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendto(user, ("127.0.0.1", 10000))
+s.close()
