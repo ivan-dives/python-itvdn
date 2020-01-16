0b5c58ecc007989c91d7179e1bee3e8afd3984db
dimamarjan <dima.marjan@gmail.com>
2020-01-16 14:55:24 +0200

    update

--- домашка/advanced/lesson 1/Dmytro Marianchenko/t_3_SMTP.py
+++ домашка/advanced/lesson 1/Dmytro Marianchenko/t_3_SMTP.py
@@ -3,7 +3,7 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("smtp.gmail.com", 587))
 sock.send(b"EHLO google.com")
-resp = sock.recv(5000)
+resp = sock.recv(1024)
 sock.close()
 resp = bytes.decode(resp)
 print()
