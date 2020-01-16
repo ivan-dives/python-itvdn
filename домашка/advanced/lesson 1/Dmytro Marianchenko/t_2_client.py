b5ac1f20e51b97d6ca643f60a24a72387e609e35
dimamarjan <dima.marjan@gmail.com>
2020-01-16 13:15:16 +0200

    update

--- домашка/advanced/lesson 1/Dmytro Marianchenko/t_2_client.py
+++ домашка/advanced/lesson 1/Dmytro Marianchenko/t_2_client.py
@@ -1,10 +1,8 @@
 import socket
 import os
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 10001
 SN = os.environ["COMPUTERNAME"]
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 MESSAGE = str.encode(SN)
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(MESSAGE, ("127.0.0.1", 10001))
