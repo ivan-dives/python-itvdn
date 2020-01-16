ee45b8edc1f7119b1607dfcf15dcc9326e00ef9e
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2020-01-16 19:33:05 +0200

    Update 18_3_EHLO.py

--- домашка/advanced/lesson 1/Pavel K/18_3_EHLO.py
+++ домашка/advanced/lesson 1/Pavel K/18_3_EHLO.py
@@ -1,7 +1,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('smtp-gmail.com', 587))
+sock.connect(('smtp.gmail.com', 587))
 text = 'EHLO google.com'
 text = str.encode(text)
 sock.send(text)
