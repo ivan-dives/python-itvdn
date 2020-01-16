693e0f20066bee09dd3955552ca44f6b66ca1974
denik-mv <levyiakk123@gmail.com>
2020-01-16 02:03:47 +0200

    update

--- домашка/advanced/lesson 1/Marukhniak Denys/L1_2_client.py
+++ домашка/advanced/lesson 1/Marukhniak Denys/L1_2_client.py
@@ -1,13 +1,18 @@
 # Создайте UDP сервер, который ожидает сообщения о новых устройствах в сети. Он принимает сообщения определенного
 # формата, в котором будет идентификаторустройства и печатает новые подключения в консоль. Создайте UDP клиента, который
 # будет отправлять уникальный идентификатор устройства на сервер, уведомляя о своем присутствии.
+import platform
 import socket
 import os
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-t = os.environ['COMPUTERNAME']
-t = t.encode()
-s.sendto(t, ('127.0.0.1', 10000))
+
+name = platform.node()
+# name = socket.gethostname()
+# name = os.environ['COMPUTERNAME']
+
+name = name.encode()
+s.sendto(name, ('127.0.0.1', 10000))
 s.close()
 exit()
