77c8ec6d9f9284c05877c19cb538d1fab8596247
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2020-01-17 12:00:58 +0200

    Add files via upload

---
+++ домашка/advanced/lesson 1/Pavel K/18_3_Google_get.py
@@ -0,0 +1,13 @@
+# по третьему заданию - открыть клиентский TCP сокет на google.com (порт 80), сказать в сокет "GET", получить ответ,
+# сохранить в файл
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('google.com', 80))
+s.send(b'GET\n')
+res = s.recv(730)
+
+with open('g1.txt', 'wb') as file:
+    file.write(res)
+
+s.close()
