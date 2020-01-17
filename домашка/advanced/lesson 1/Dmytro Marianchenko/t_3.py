3c1df729dae1cfbca4fda1d5605579c764fdb0de
dimamarjan <dima.marjan@gmail.com>
2020-01-17 11:28:24 +0200

    update

--- домашка/advanced/lesson 1/Dmytro Marianchenko/t_3.py
+++ домашка/advanced/lesson 1/Dmytro Marianchenko/t_3.py
@@ -3,9 +3,8 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("google.com", 80))
 sock.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
-resp = sock.recv(1024)
+resp = sock.recv(1024).decode("utf-8")
 sock.close()
-resp = bytes.decode(resp)
 with open("google_answer.txt", "w") as f:
     f.write(resp)
 print()
