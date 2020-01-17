0b627900fd98cdd5b0da28231db2a564bcad642c
Александр <apamic1995@gmail.com>
2020-01-17 20:39:07 +0200

    Chat server

--- домашка/advanced/lesson 1.2/Alex Shvab/HW Dop Chat/server.py
+++ домашка/advanced/lesson 1.2/Alex Shvab/HW Dop Chat/server.py
@@ -3,14 +3,14 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('127.0.0.1', 5050))
-client = []
+list_clients = []
 print('Start Server')
 while 1:
     data, addr = sock.recvfrom(1024)
     print(addr[0], addr[1])
-    if addr not in client:
-        client.append(addr)
-    for clients in client:
+    if addr not in list_clients:
+        list_clients.append(addr)
+    for clients in list_clients:
         if clients == addr:
             continue
         sock.sendto(data, clients)
