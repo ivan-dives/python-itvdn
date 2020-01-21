4c295471624505afa16f9ee7c137fac7380c5003
Александр <apamic1995@gmail.com>
2020-01-21 16:33:24 +0200

    Chat client

--- домашка/advanced/lesson 1.2/Alex Shvab/HW Dop Chat/user.py
+++ домашка/advanced/lesson 1.2/Alex Shvab/HW Dop Chat/user.py
@@ -1,20 +1,20 @@
 import socket
-import threading
-
-
-def read_sok():
-    while 1:
-        data = s.recv(1024)
-        print(data.decode('utf-8'))
+import sys
+import select
 
 
 server = ('127.0.0.1', 5050)
-alias = input("Enter your name: ")
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(server)
-s.sendto((alias + ' Connect to server').encode('utf-8'), server)
-thread = threading.Thread(target=read_sok)
-thread.start()
-while 1:
-    msg = input()
-    s.sendto(('[' + alias + ']' + msg).encode('utf-8'), server)
+name = input("Enter your name: ")
+user_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+user_sock.connect(server)
+
+while True:
+    r, w, e = select.select([user_sock, sys.stdin], [], [], 1)
+    user_sock.send((name + ' Connect to server').encode('utf-8'))
+    if user_sock in r:
+        msg = user_sock.recv(5000)
+        msg = msg.decode("utf-8")
+        print(msg)
+    if sys.stdin in r:
+        msg = sys.stdin.readline()
+        user_sock.send(('[' + name + '] ' + msg).encode('utf-8'))
