46f925d81faf7977f6cd7d6b7fe9ff8aae30f359
Александр <apamic1995@gmail.com>
2020-01-22 14:06:49 +0200

    Chat client

--- домашка/advanced/lesson 1.2/Alex Shvab/HW Dop Chat/user.py
+++ домашка/advanced/lesson 1.2/Alex Shvab/HW Dop Chat/user.py
@@ -3,18 +3,19 @@ import sys
 import select
 
 
-server = ('127.0.0.1', 5050)
-name = input("Enter your name: ")
+server = ('127.0.0.1', 20000)
+#name = input("Enter your name: ")
 user_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 user_sock.connect(server)
 
 while True:
     r, w, e = select.select([user_sock, sys.stdin], [], [], 1)
-    user_sock.send((name + ' Connect to server').encode('utf-8'))
+    #user_sock.send((name + ' Connect to server').encode('utf-8'))
     if user_sock in r:
-        msg = user_sock.recv(5000)
+        msg = user_sock.recv(1024)
         msg = msg.decode("utf-8")
         print(msg)
     if sys.stdin in r:
         msg = sys.stdin.readline()
-        user_sock.send(('[' + name + '] ' + msg).encode('utf-8'))
+        #user_sock.send(('[' + name + '] ' + msg).encode('utf-8'))
+        user_sock.send(msg.encode('utf-8'))
