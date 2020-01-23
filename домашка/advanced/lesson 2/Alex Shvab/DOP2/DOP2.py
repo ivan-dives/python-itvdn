2a9fa842a96cf48be43e429f4f4fc7f1f43853e9
Александр <apamic1995@gmail.com>
2020-01-23 19:33:34 +0200

    Create sqlite DB

---
+++ домашка/advanced/lesson 2/Alex Shvab/DOP2/DOP2.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect("mydb.sqlite3")
+
+conn.execute('CREATE TABLE "material"(id, weight REAL, height REAL, add_character)')
+conn.execute(
+    """INSERT INTO material(id, weight, height, add_character)
+       VALUES (1, 5.2, 1, "[('width', 3), ('netto', 0.8)]"),
+              (2, 3, 5.2, "[('width', 2), ('netto', 0.2)]"),
+              (3, 1000, 1.5, "[('wheels', 4), ('passengers', 5)]")
+    """
+)
+
+mat = conn.execute('SELECT * FROM "material"').fetchall()
+print(mat)
+
+conn.close()
