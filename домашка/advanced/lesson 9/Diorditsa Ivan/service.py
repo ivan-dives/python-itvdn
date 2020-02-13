2bdfc423cf66ae5be1b5e7fbcfe08969802d2734
Ivan Diorditsa <ivan.diorditsa@gmail.com>
2020-02-13 21:47:17 +0200

    сервер к практическому заданию 13.02

--- домашка/advanced/lesson 9/Diorditsa Ivan/service.py
+++ домашка/advanced/lesson 9/Diorditsa Ivan/service.py
@@ -1,4 +1,9 @@
 import socketserver
+import threading
+import random
+
+clients = dict()
+clients_lock = threading.Lock()
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
@@ -11,29 +16,40 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     """
 
     def handle(self):
-        # (b'text', <socket.socket fd=3, family=AddressFamily.AF_INET, type=SocketKind.SOCK_DGRAM, proto=0, laddr=('127.0.0.1', 9999)>)
-        # print(self.request[0].decode())
-        # self.request[1].sendto(b"PASSWORD ACCEPTED", self.client_address)
-        # import time
-        # time.sleep(3)
-        # self.request[1].sendto(b"your balance is 1000 UAH", self.client_address)
-        # return
+        try:
+            word = self.request.recv(10).decode()
+            print(f'got client, {word=}')
 
-        # self.request is the TCP socket connected to the client
-        # self.data = self.request.recv(1024).strip()
-        # print("{} wrote:".format(self.client_address[0]))
-        # print(self.data)
-        # # just send back the same data, but upper-cased
-        # self.request.sendall(self.data.upper())
+            clients_lock.acquire()
+            client = clients.setdefault('word', {'cond': threading.Condition(), 'count': 0, 'sum': 0})
+            clients_lock.release()
 
-        print(self.request.recv(1024).decode())
-        self.request.send(b'ACCEPTED')
+            num = random.randint(0, 100)
+            client['cond'].acquire()
+            client['count'] += 1
+            if client['count'] < 5:
+                client['cond'].wait()
+            else:
+                client['cond'].notify_all()
+            client['sum'] += num
+            sum = client['sum']
+            client['cond'].release()
+
+            print(f'sent {num=} to {word=}, {sum=}')
+            self.request.send(f"{num}".encode())
+        except Exception as ex:
+            print(ex)
+
+
+class MyTCPServer(socketserver.ThreadingTCPServer):
+    allow_reuse_address = True
+    request_queue_size = 10
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "0.0.0.0", 10000
 
     # Create the server, binding to localhost on port 9999
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+    with MyTCPServer((HOST, PORT), MyTCPHandler) as server:
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
         server.serve_forever()
